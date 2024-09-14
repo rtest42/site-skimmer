098ab41203f78317d5132d87df7037dfa10c798d
Michael Liu <miyliu@ucdavis.edu>
2024-09-13 17:01:56 -0700

    Same as Colab

--- skimmer.py
+++ skimmer.py
@@ -13,7 +13,11 @@ from keras.models import Model
 import numpy as np
 import cv2
 # For clothing segment
+from transformers import pipeline
+from PIL import Image
 
+# Initialize segmentation pipeline
+segmenter = pipeline(model="mattmdjaga/segformer_b2_clothes")
 
 # Returns a list of matching patterns
 def get_files(patterns):
@@ -48,7 +52,7 @@ def check_image_link(image_url):
 # Image Pre-check before saving to disk
 def precheck_image(image):
     # Skip files less than 8kB
-    if len(image) >= 8000:
+    if (len(image) >= 8000):
         # Skip grayscale images
         img = Image.open(BytesIO(image))
         img = img.convert('RGB')
@@ -89,6 +93,72 @@ def check_for_duplicates(base_image_path, comparison_image_path):
         return True
     return False
 
+# Given a decimal convert to bitmap
+def parse_seg_mode(value):
+    options = ["Hat", "Upper-clothes", "Skirt", "Pants", "Dress", "Belt", "Left-shoe", "Right-shoe", "Scarf"]
+    output = []
+    value_binary = str(bin(value)[2:].zfill(len(options)))
+
+    for i in range(len(value_binary)):
+        if (value_binary[i] == '1'):
+            output.append(options[i])
+
+    return output
+
+def segment_clothing(img, clothes= ["Hat", "Upper-clothes", "Skirt", "Pants", "Dress", "Belt", "Left-shoe", "Right-shoe", "Scarf"]):
+    # Segment image
+    segments = segmenter(img)
+
+    # Create list of masks
+    mask_list = []
+    for s in segments:
+        if(s['label'] in clothes):
+            mask_list.append(s['mask'])
+
+
+    # Paste all masks on top of eachother
+    final_mask = np.array(mask_list[0])
+    for mask in mask_list:
+        current_mask = np.array(mask)
+        final_mask = final_mask + current_mask
+
+    # Convert final mask from np array to PIL image
+    final_mask = Image.fromarray(final_mask)
+
+    # Apply mask to original image
+    img.putalpha(final_mask)
+
+    return img
+
+
+def batch_segment_clothing(img_dir, out_dir, clothes= ["Hat", "Upper-clothes", "Skirt", "Pants", "Dress", "Belt", "Left-shoe", "Right-shoe", "Scarf"]):
+    # Create output directory if it doesn't exist
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # Iterate through each file in the input directory
+    for filename in os.listdir(img_dir):
+        if filename.endswith(".jpg") or filename.endswith(".JPG") or filename.endswith(".png") or filename.endswith(".PNG"):
+            try:
+                # Load image
+                img_path = os.path.join(img_dir, filename)
+                img = Image.open(img_path).convert("RGBA")
+
+                # Segment clothing
+                segmented_img = segment_clothing(img, clothes)
+
+                # Save segmented image to output directory as PNG
+                out_path = os.path.join(out_dir, filename.split('.')[0] + ".png")
+                segmented_img.save(out_path)
+
+                print(f"Segmented {filename} successfully.")
+
+            except Exception as e:
+                print(f"Error processing {filename}: {e}")
+
+        else:
+            print(f"Skipping {filename} as it is not a supported image file.")
+
 # Downloads images and its content
 def download_images(html_text, image_label, counter=0):
     image_metadata = []
@@ -125,33 +195,23 @@ def download_images(html_text, image_label, counter=0):
                 with open(image_name, 'wb') as f:
                     f.write(image_data)
 
-                print("Wrote image " +str(counter))
+                print("Fetched image " + str(counter))
                 # Check if image is a duplicate (using five images before as comparison)
-                if (counter > 5):
-                    duplicate_found = False
-                    for i in range(5):
-                        image_to_check = image_label + str(counter - i) + file_extension
 
-                        if check_for_duplicates(image_name, "./" + image_label + "/" + image_to_check):
-                            duplicate_found = True
-                            os.remove(image_name)
-                            break
+                # Save item metadata
+                image_info = {
+                    'img_url': image_url,
+                    'img_name': image_name,
+                    'img_label': image_label
+                }
+                image_metadata.append(image_info)
 
-                    if not duplicate_found:
-                        # Save item metadata
-                        image_info = {
-                            'img_url': image_url,
-                            'img_name': image_name,
-                            'img_label': image_label
-                        }
-                        image_metadata.append(image_info)
-
-                        # Create file for Google Vertex
-                        vertex = {
-                            'img_dir': "gs://cloud-ml-data/{}".format(image_name),  # Change directory as necessary
-                            'label': image_label
-                        }
-                        google_vertex.append(vertex)
+                # Create file for Google Vertex
+                vertex = {
+                    'img_dir': "gs://cloud-ml-data/{}".format(image_name),  # Change directory as necessary
+                    'label': image_label
+                }
+                google_vertex.append(vertex)
 
                 counter += 1
 
@@ -166,12 +226,13 @@ def download_images(html_text, image_label, counter=0):
 # The output consists of an image folder and several CSV files
 def main():
     # Check for arguments
-    if len(sys.argv) < 2:
-        print("Usage: python3 skimmer.py <pattern1> [pattern2] ...")
+    if len(sys.argv) < 3:
+        print("Usage: python3 skimmer.py <segment_mode> <pattern1> [pattern2] ...")
         sys.exit(1)
 
     # Get list of files
-    args = sys.argv[1:]
+    seg_mode = parse_seg_mode(int(sys.argv[3]))
+    args = sys.argv[4:]
     files = get_files(args)
 
     if files is None:
@@ -192,6 +253,15 @@ def main():
 
             # Fetch metadata
             metadata = download_images(parsed_data, label, image_count)
+            # Create a directory to save images
+            label_name = label.replace(' ', '_').lower()
+            os.makedirs(label_name, exist_ok=True)
+            input_dir = label_name
+            os.makedirs(label_name + "_f", exist_ok=True)
+            output_dir = label_name + "_f"
+            # ["Hat", "Upper-clothes", "Skirt", "Pants", "Dress", "Belt", "Left-shoe", "Right-shoe", "Scarf"]
+            #    0          0             0        1        0        0         0             0          0
+            batch_segment_clothing(input_dir, output_dir, clothes=seg_mode)
 
             # Update counter
             image_count = metadata['counter']
@@ -199,6 +269,3 @@ def main():
             save_to_csv(metadata['image_metadata'], 'items_metadata_{}.csv'.format(label.replace(' ', '_').lower()))
             save_to_csv(metadata['google_vertex'], 'google_vertex_{}.csv'.format(label.replace(' ', '_').lower()))
 
-
-if __name__ == "__main__":
-    main()
