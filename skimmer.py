f79b3cb2452adcaadc4d796c2f7be6a8008a6c8e
Royce Li <royce95129@gmail.com>
2024-09-20 20:59:12 -0700

    Added multiprocessing and multithreading

--- skimmer.py
+++ skimmer.py
@@ -3,12 +3,14 @@ import csv
 import itertools
 import numpy as np  # Duplicate images
 import os
+import re  # Regex
 import requests
 import sys
+import threading
 import time
 import torch.cuda
 from bs4 import BeautifulSoup
-from concurrent.futures import ThreadPoolExecutor  # Threading
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor  # Multithreading, multiprocessing
 from glob import glob
 from io import BytesIO
 # For duplicate images
@@ -19,7 +21,7 @@ from keras.models import Model
 from PIL import Image
 from transformers import pipeline
 
-start_time = time.time()
+start_time = time.perf_counter()
 
 # Prefer GPU (0) over CPU (-1)
 device = 0 if torch.cuda.is_available() else -1
@@ -27,14 +29,14 @@ device = 0 if torch.cuda.is_available() else -1
 segmenter = pipeline(model="mattmdjaga/segformer_b2_clothes", device=device)
 
 # Get list of clothing categories
-with open('categories.txt', 'r') as f:
-    clothing_categories = f.readlines()
+with open('categories.txt', 'r') as clothing_file:
+    clothing_categories = clothing_file.readlines()
 
 # Load the VGG16 model pre-trained on ImageNet
+weights = 'imagenet'
 if os.path.isfile('vgg16_weights_tf_dim_ordering_tf_kernels.h5'):
-    base_model = VGG16(weights='vgg16_weights_tf_dim_ordering_tf_kernels.h5')
-else:
-    base_model = VGG16(weights='imagenet')
+    weights = 'vgg16_weights_tf_dim_ordering_tf_kernels.h5'
+base_model = VGG16(weights=weights)
 model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
 
 
@@ -91,7 +93,25 @@ def extract_features(img_path, image_model):
     img_data = np.expand_dims(img_data, axis=0)
     img_data = preprocess_input(img_data)
     features = image_model.predict(img_data)
-    return features
+    # Get image number from path
+    # Images may not be finished in order, so add a key
+    image_key = re.search(r"\d+\.\D{3,4}$", img_path)
+    image_key = image_key.group(0)
+    image_key = image_key.partition('.')[0]
+    image_key = int(image_key)
+    return {image_key: features}  # Return a key as well
+
+
+# Function for getting image vectors using multiprocessing
+def extract_features_multiprocessing(image_name) -> list:
+    image_vectors = []
+    with ProcessPoolExecutor() as executor:
+        futures = {executor.submit(extract_features, name, model) for name in image_name}
+
+    for future in futures:
+        image_vectors.append(future.result())
+
+    return image_vectors
 
 
 # Function to check image similarity
@@ -115,6 +135,7 @@ def parse_seg_mode(value):
     return output
 
 
+# Helper function for segmenting clothing
 def segment_clothing(img, clothes=None):
     clothes = clothes or clothing_categories
     # Segment image
@@ -141,42 +162,49 @@ def segment_clothing(img, clothes=None):
     return img
 
 
-def batch_segment_clothing(img_dir, out_dir, clothes=None):
+# Save segmented clothing
+def batch_segment_clothing(filename, img_dir, out_dir, clothes) -> None:
+    if filename[filename.find('.'):] not in ['.jpg', '.JPG', '.png', '.PNG']:
+        print("Skipping", filename, "as it is not a supported image file.")
+        return
+
+    # Iterate through each file in the input directory
+    try:
+        # Load image
+        img_path = os.path.join(img_dir, filename)
+        img = Image.open(img_path).convert("RGBA")
+
+        # Segment clothing
+        segmented_img = segment_clothing(img, clothes)
+
+        # Save segmented image to output directory as PNG
+        out_path = os.path.join(out_dir, filename.split('.')[0] + ".png")
+        segmented_img.save(out_path)
+
+        print("Segmented", filename, "successfully.")
+    except IndexError as e:
+        print("Error processing", filename, ":", e)
+
+
+# Segment multiple images
+def batch_segment_clothing_multiprocessing(img_dir, out_dir, clothes=None) -> None:
     clothes = clothes or clothing_categories
     # Create output directory if it doesn't exist
     os.makedirs(out_dir, exist_ok=True)
 
-    # Iterate through each file in the input directory
-    for filename in os.listdir(img_dir):
-        if filename[filename.find('.'):] in ['.jpg', '.JPG', '.png', '.PNG']:
-            try:
-                # Load image
-                img_path = os.path.join(img_dir, filename)
-                img = Image.open(img_path).convert("RGBA")
-
-                # Segment clothing
-                segmented_img = segment_clothing(img, clothes)
-
-                # Save segmented image to output directory as PNG
-                out_path = os.path.join(out_dir, filename.split('.')[0] + ".png")
-                segmented_img.save(out_path)
-
-                print(f"Segmented {filename} successfully.")
-            except IndexError as e:
-                print(f"Error processing {filename}: {e}")
-        else:
-            print(f"Skipping {filename} as it is not a supported image file.")
+    with ProcessPoolExecutor() as executor:
+        [executor.submit(batch_segment_clothing, filename, img_dir, out_dir, clothes) for filename in os.listdir(img_dir)]
 
 
 # Helper function for downloading images
-def download_image(image_url, image_label, counter, image_model, image_vectors):
+def download_image(image_url, image_label, counter, counter_lock):
     # Make image URL valid
-    image_url = check_image_link(image_url)
+    image_url = check_image_link(image_url.get('src'))
 
     # Skip files with bad file extension (.svg)
     file_extension = os.path.splitext(image_url)[-1].lower()
     if file_extension.find('.svg') != -1:
-        return
+        return None
 
     # Add missing file extension (.jpg for now)
     if file_extension.find('.') == -1:
@@ -187,29 +215,24 @@ def download_image(image_url, image_label, counter, image_model, image_vectors):
         image_data = requests.get(image_url).content
     except requests.exceptions.InvalidURL as e:
         print(e)
-        return
+        return None
 
+    # Check image before saving to disk
     if not pre_check_image(image_data):
-        return
+        return None
 
-    num = next(counter)
-    image_name = os.path.join(image_label, f"{image_label}{num}{file_extension}")
+    # Name file
+    # Lock thread to prevent race condition for counter
+    with counter_lock:
+        image_name = os.path.join(image_label, f"{image_label}{counter[0]}{file_extension}")
+        print("Fetched image", counter[0])
+        counter[0] += 1  # Safely update counter
+
+    # Write image
     with open(image_name, 'wb') as f:
         f.write(image_data)
 
-    # Get image vector
-    feature = extract_features(image_name, image_model)
-    # Check if image is a duplicate (using five images before as comparison)
-    # for image_vector in image_vectors:  # [-5:]:
-    #    if cosine_similarity(feature, image_vector) > 0.8:
-    #        print(f"Found duplicate image {counter}")
-    #        os.remove(image_name)
-    #        return
-
-    # Unique image
-    image_vectors.append(feature)
-    print(f"Fetched image {num}")
-
+    # Metadata
     return {
         'img_url': image_url,
         'img_name': image_name,
@@ -218,24 +241,21 @@ def download_image(image_url, image_label, counter, image_model, image_vectors):
 
 
 # Downloads images and its content
-def download_images(html_text, image_label, image_model):
-    image_vectors = []
+def download_images(images, image_label):
     image_metadata = []
     google_vertex = []
-    counter = itertools.count(0)
-
+    counter = [0]  # Make the counter mutable
+    counter_lock = threading.Lock()
     # Create a directory to save images
     os.makedirs(image_label, exist_ok=True)
 
-    images = html_text.find_all('img')
     # Utilize threading to download multiple images at once
-    with ThreadPoolExecutor() as executor:
-        future_to_image = {executor.submit(download_image, img.get('src'), image_label, counter, model, image_vectors) for img in images}
+    with ThreadPoolExecutor(max_workers=len(images)) as executor:
+        futures = {executor.submit(download_image, img, image_label, counter, counter_lock) for img in images}
 
-    print(len(image_vectors))
-    # Get data for CSV files
-    for future in future_to_image:
+    for future in futures:
         metadata = future.result()
+        # Append metadata to CSV
         if metadata:
             image_metadata.append(metadata)
             google_vertex.append({
@@ -249,54 +269,126 @@ def download_images(html_text, image_label, image_model):
     }
 
 
+# Helper function for opening and extracting HTML contents from a file
+def extract_tags(file, tag='img') -> list:
+    with open(file, 'r', encoding='utf8') as f:
+        print("Opening", file)
+        # Read and parse text
+        data = f.read()
+        parsed_data = BeautifulSoup(data, 'html.parser')
+        # List all tags' properties
+        return parsed_data.find_all(tag)
+
+
+# Get a list of tags from multiple files
+def extract_tags_multithreading(files) -> list:
+    tags = []
+    with ThreadPoolExecutor(max_workers=len(files)) as executor:
+        # Open multiple files concurrently
+        results = executor.map(extract_tags, files)
+
+    for result in results:
+        tags.extend(result)
+
+    return tags
+
+
+# Flag all duplicate images
+def list_duplicates(image_vectors, num_images, threshold=0.80) -> set[int]:
+    duplicates = set()
+    for i in range(num_images):
+        # Skip those already flagged
+        if i in duplicates:
+            continue
+
+        for j in range(i + 1, num_images):
+            # Skip those already flagged
+            if j in duplicates:
+                continue
+            # Compare similarity
+            similarity = cosine_similarity(image_vectors[i], image_vectors[j])
+            if similarity > threshold:
+                print("Duplicate detected:", i, "and", j)
+                duplicates.add(j)
+
+    return duplicates
+
+
+# Remove duplicate images
+def remove_duplicates(duplicate_set, label) -> None:
+    with ThreadPoolExecutor() as executor:
+        executor.map(os.remove, glob(f"{itertools.repeat(label)}/*{duplicate_set}.*"))
+
+
 # The main function receives HTML files as arguments and a label as an input
 # The output consists of an image folder and several CSV files
-def main():
+def main() -> int:
     # Check for arguments
     if len(sys.argv) < 3:
         print("Usage: python3 skimmer.py <segment_mode> <pattern1> [pattern2] ...")
-        sys.exit(1)
+        return 1
+
+    # Get segmentation mode (number)
+    # Common bitmap values:
+    # 6 - shoes
+    # 16 - dresses
+    # 96 - skirt, pants, lower-body clothing
+    # 128 - shirt, jacket, upper-body clothing
+    try:
+        seg_mode = parse_seg_mode(int(sys.argv[1]))
+    except ValueError:
+        print("segment_mode has to be a number.")
+        return 1
 
     # Get list of files
-    seg_mode = parse_seg_mode(int(sys.argv[1]))
     files = get_files(sys.argv[2:])
-
     if files is None:
         print("ERROR: No files found!")
-        sys.exit(1)
+        return 1
 
-    label = input("Enter a label: ")
-    image_count = 0
+    # Output directory
+    while True:
+        label = input("Enter a label (q to quit): ")
+        # Ensure user input is valid
+        if not label:
+            print("Label should not be empty!")
+        elif label[-1].isdigit():
+            print("Label should not end in a digit! (for counting purposes)")
+        elif label.lower() in ['q', 'quit']:
+            print("Exiting program.")
+            return 1
+        else:
+            break
 
-    # Open and read all the text files
-    for file in files:
-        with open(file, 'r', encoding="utf8") as f:
-            print(f"Opening {file}")
+    # Get image tags from all inputted files
+    image_tags = extract_tags_multithreading(files)
 
-            # Read and parse text
-            data = f.read()
-            parsed_data = BeautifulSoup(data, 'html.parser')
+    # Download the images
+    image_metadata = download_images(image_tags, label)
+    image_counter = [len(image_metadata['image_metadata'])]
 
-            # Fetch metadata
-            metadata = download_images(parsed_data, label, model)
-            # Create a directory to save images
-            label_name = label
-            os.makedirs(label_name, exist_ok=True)
-            input_dir = label_name
-            os.makedirs(label_name + "_f", exist_ok=True)
-            output_dir = label_name + "_f"
-            # ["Hat", "Upper-clothes", "Skirt", "Pants", "Dress", "Belt", "Left-shoe", "Right-shoe", "Scarf"]
-            #    0          0             0        1        0        0         0             0          0
-            batch_segment_clothing(input_dir, output_dir, clothes=seg_mode)
+    # Load images onto image vectors
+    image_directories = [image_name['img_name'] for image_name in image_metadata['image_metadata']]
+    image_vectors = extract_features_multiprocessing(image_directories)
 
-            # Update counter
-            # image_count = metadata['counter']
-            # Put images into CSV file
-            save_to_csv(metadata['image_metadata'], f"items_metadata_{label}.csv")
-            save_to_csv(metadata['google_vertex'], f"google_vertex_{label}.csv")
+    # Select all images that are duplicates, then remove them
+    image_duplicates = list_duplicates(image_vectors, image_counter)
+    remove_duplicates(image_duplicates, label)
+
+    # Segment clothing and put them into another folder
+    # ["Hat", "Upper-clothes", "Skirt", "Pants", "Dress", "Belt", "Left-shoe", "Right-shoe", "Scarf"]
+    #    0          0             0        1        0        0         0             0          0
+    batch_segment_clothing_multiprocessing(label, f"{label}_f", seg_mode)
+
+    # Put images into CSV file
+    save_to_csv(image_metadata['image_metadata'], f"items_metadata_{label}.csv")
+    save_to_csv(image_metadata['google_vertex'], f"google_vertex_{label}.csv")
+
+    # Indicate successful run
+    return 0
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    main()
-    print("Done. Time elapsed: ", time.time() - start_time, " seconds")
+    exit_code = main()  # Run program
+    print("Program finished with exit code", exit_code)
+    print("Done. Time elapsed:", time.perf_counter() - start_time, "seconds")
