35f79ded04399c5b8bdd5a8b6c20dd7fe77931b9
rtest42 <royce95129@gmail.com>
2024-10-18 00:41:19 -0700

    test for puppeteer

---
+++ test.js
@@ -0,0 +1,61 @@
+import puppeteer from 'puppeteer-core';
+
+//const puppeteer = require('puppeteer-core');
+/*
+(async () => {
+  const browser = await puppeteer.launch({
+    //channel: 'edge',  // You can also use 'chrome', 'chrome-beta', 'chromium', 'edge', etc.
+    executablePath: 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe'
+  });
+  const page = await browser.newPage();
+  await page.goto('https://developer.chrome.com/');
+
+    // Set screen size.
+    await page.setViewport({width: 1080, height: 1024});
+
+    // Type into search box.
+    //await page.locator('.devsite-search-field').fill('automate beyond recorder');
+
+    // Wait and click on first result.
+    //await page.locator('.devsite-result-item-link').click();
+
+    // Locate the full title with a unique string.
+    //const textSelector = await page
+    //.locator('text/Customize and automate')
+    //.waitHandle();
+    //const fullTitle = await textSelector?.evaluate(el => el.textContent);
+
+    // Print the full title.
+    //console.log('The title of this blog post is "%s".', fullTitle);
+
+    await browser.close();
+})();*/
+// Launch the browser and open a new blank page
+const browser = await puppeteer.launch({
+    //channel: 'edge',  // You can also use 'chrome', 'chrome-beta', 'chromium', 'edge', etc.
+    executablePath: 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe'
+  });
+const page = await browser.newPage();
+
+// Navigate the page to a URL.
+await page.goto('https://developer.chrome.com/');
+
+// Set screen size.
+await page.setViewport({width: 1080, height: 1024});
+
+// Type into search box.
+await page.locator('.devsite-search-field').fill('automate beyond recorder');
+
+// Wait and click on first result.
+await page.locator('.devsite-result-item-link').click();
+
+// Locate the full title with a unique string.
+const textSelector = await page
+  .locator('text/Customize and automate')
+  .waitHandle();
+const fullTitle = await textSelector?.evaluate(el => el.textContent);
+
+// Print the full title.
+console.log('The title of this blog post is "%s".', fullTitle);
+
+await browser.close();
