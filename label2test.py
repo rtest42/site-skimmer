f3251347af138d16985898a4084fe0b4b9db2c23
Royce Li <76494636+rtest42@users.noreply.github.com>
2024-12-29 21:19:14 -0800

    Add input when running program

--- label2test.py
+++ label2test.py
@@ -5,13 +5,25 @@ from pinscrape import scraper, Pinterest
 from transformers import pipeline
 
 
-keyword = "full body model"
-output_folder = "output"
-number_of_workers = os.cpu_count() or 1
-images_to_download = 200
+# keyword = "full body clothing model"
+# output_folder = "label2test"
+# number_of_workers = os.cpu_count() or 1
+# images_to_download = 100
+# proxies = {}
 
 
 def main(args):
+    keyword = input("Enter your search keywords: ")
+    output_folder = input("Enter your image directory: ")
+    images_to_download = input("Enter the number of images to download (default 100): ")
+    if len(images_to_download.strip()) == 0:
+        images_to_download = 100
+    else:
+        images_to_download = int(images_to_download)
+    proxies = {}
+    number_of_workers = os.cpu_count() or 1
+
+    details = scraper.scrape(keyword, output_folder, proxies, number_of_workers, images_to_download)
     p = Pinterest(user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.2 Safari/605.1.15")
     images_url = p.search(keyword, images_to_download)
     print(images_url)
