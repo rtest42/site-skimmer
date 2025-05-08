2549f44f1bc9946bcecc3b00101f199e4cf3235c
rtest42 <royce95129@gmail.com>
2025-05-08 13:50:25 -0700

    Update SSCD.py

--- SSCD.py
+++ SSCD.py
@@ -4,7 +4,7 @@ import torch
 import threading
 import os
 import sys
-import numpy as np
+# import numpy as np
 import requests
 import subprocess
 from concurrent.futures import as_completed, ThreadPoolExecutor
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 
 class SSCD(object):
-    def __init__(self, label: str):
+    def __init__(self, label: str) -> None:
         self.label = label
         self.categories = ["Hat", "Upper-clothes", "Skirt", "Pants", "Dress",
                            "Belt", "Left-shoe", "Right-shoe", "Scarf"]
@@ -26,7 +26,7 @@ class SSCD(object):
         self.pipe = pipeline("image-segmentation", model="mattmdjaga/segformer_b2_clothes", device=device)
         self.dataset = None
 
-    def load_dataset(self):
+    def load_dataset(self) -> None:
         self.dataset = load_dataset("imagefolder", data_dir=self.label, split='test')
         # Workaround to keep file paths
         # Prevent decoding, keep file paths
@@ -39,7 +39,7 @@ class SSCD(object):
         self.dataset = self.dataset.cast_column("image", DatasetImage(decode=True))
 
     @staticmethod
-    def download_image(session: requests.Session, url: str, label: str, counter: list[int], lock: threading.Lock):
+    def download_image(session: requests.Session, url: str, label: str, counter: list[int], lock: threading.Lock) -> None:
         headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0'
@@ -64,7 +64,7 @@ class SSCD(object):
         except requests.exceptions.RequestException as e:
             print(f"Failed to download {url}: {e}")  # TODO convert to logging
 
-    def download_images(self, links: list[str], label: str):
+    def download_images(self, links: list[str], label: str) -> None:
         os.makedirs(label, exist_ok=True)
         session = requests.Session()
         counter = [0, len(links)]  # [counter, number of links (constant)]
@@ -77,7 +77,7 @@ class SSCD(object):
                     # result = future.result()
                     pbar.update(1)
 
-    def skimmer(self, searches: list[str], folders: list[str], rounds: int):
+    def skimmer(self, searches: list[str], folders: list[str], rounds: int) -> None:
         # Skimming is slow on purpose to prevent anti-bot detection
         for search, folder in zip(searches, folders):
             print(f"Searching {search} via Pinterest")
@@ -86,66 +86,85 @@ class SSCD(object):
             filtered_results = [url for url in result if '236x' in url]  # Only keep images with a width of 236px
             self.download_images(filtered_results, os.path.join(self.label, 'test', folder))
 
-    def image_segmentation(self):
+    def edge_detection(self, img: Image, threshold: float = 0.2) -> bool:
+        pass
+
+    def get_color_percentage(self, img: Image, clr: tuple = (255, 255, 255)) -> float:
+        pass
+
+    def image_segmentation(self) -> None:
         # Make directories
         for label_name in self.dataset.features['label'].names:
             for category in ["bad", "good", "output"]:
                 os.makedirs(os.path.join(self.label, category, label_name), exist_ok=True)
 
         images = KeyDataset(self.dataset, "image")
+        for i, output in tqdm(enumerate(self.pipe(images)), total=len(self.dataset), desc="Segmenting images"):
+            data = self.dataset[i]
+            image = data['image']
+            filename = os.path.basename(data['filename']['path'])
+            label_name = data['label-name']
+            masks = {segment['label']: segment['mask'] for segment in output}
 
-        for i, image in tqdm(enumerate(self.pipe(images)), total=len(self.dataset), desc="Segmenting images"):
-            filename = self.dataset[i]['filename']['path']
-            label_name = self.dataset[i]['label-name']
-            filename = os.path.basename(filename)
+            if self.label == 'label1':
+                upper_body = masks.get('Upper-clothes')
+                lower_body = masks.get('Pants')
+                if upper_body and lower_body:
+                    pass
+                elif upper_body and not lower_body:
+                    pass
+                elif not upper_body and lower_body:
+                    pass
+                else:
+                    # TODO log image masks unsuccessful
+                    continue
+            elif self.label == 'label2':
+                # Test if background surrounds main image completely
+                background = masks.get('Background')
+                if background:
+                    # TODO check if inverse of background touches edge
+                    # Inverse mask
+                    background = background.point(lambda x: 255 - x)
+                    # image.putalpha(background)
 
-            # TODO work on logic below until end of loop/function
-            output = image
-
-            # Append specified masks to output
-            masks = []
-            labels = []
-            for segment in output:
-                label = segment['label']
-                labels.append(label)
-                if label in self.categories:
-                    masks.append(segment['mask'])
-
+            image.save(os.path.join(self.label, 'output', label_name, filename))
             # Logic for handling masks
             # Detection
-            category = 'bad'
-            if 'Face' in labels and ('Left-shoe' in labels or 'Right-shoe' in labels):
-                category = 'good'
+            #category = 'bad'
+            #if 'Face' in labels and ('Left-shoe' in labels or 'Right-shoe' in labels):
+            #    category = 'good'
 
-            image.save(os.path.join(category, label_name, filename))
+            #image.save(os.path.join(category, label_name, filename))
 
             # Clipping (only for Label 1)
-            if self.label == '1':
-                if len(masks) == 0:
-                    print("Skipping-no masks detected")  # TODO change to log
-                    continue
+            #if self.label == 'label1':
+            #    if len(masks) == 0:
+            #        print("Skipping-no masks detected")  # TODO change to log
+            #        continue
 
-                stack = np.array(masks[0])
-                for mask in masks:
-                    stack += np.array(mask)
+            #    stack = np.array(masks[0])
+            #    for mask in masks:
+            #        stack += np.array(mask)
 
-                image.putalpha(Image.fromarray(stack))
-                image.save(os.path.join('output', label_name, filename))
+            #    image.putalpha(Image.fromarray(stack))
+            #    image.save(os.path.join(self.label, 'output', label_name, filename))
 
 
-def main():
-    # Input for AI Label One or Two
+def main() -> int:
     ai_label = input("AI Label 1 or 2? (1/2): ").strip()
     if ai_label not in ('1', '2'):
-        # TODO: Log bad input?
-        exit(1)
+        print("Input is not 1 or 2", file=sys.stderr)
+        return 1
 
-    method = input("Perform skimming in addition to detection (and clipping for Label 1)? (y/n): ").strip().lower()
     sscd = SSCD(f"label{ai_label}")
-
+    method = input("Perform skimming in addition to detection (and clipping for Label 1)? (y/n): ").strip().lower()
     # Perform skimmer
     if method in ('y', 'yes'):
-        search = input("Enter your search terms (separate by comma): ").strip()  # TODO: Log empty or bad input?
+        search = input("Enter your search terms (separate by comma): ").strip()
+        if len(search) == 0:
+            print("Search terms are empty", file=sys.stderr)
+            return 1
+
         rounds = int(input("Enter the number of rounds for sifting: "))
         folder = search.replace(' ', '-')
         search = search.split(',')
@@ -155,6 +174,7 @@ def main():
     sscd.load_dataset()
     # Perform segmentation; includes detection and clipping
     sscd.image_segmentation()
+    return 0
 
 
 if __name__ == '__main__':
@@ -164,4 +184,5 @@ if __name__ == '__main__':
         stream=sys.stderr
     )
 
-    main()
+    code = main()
+    print(f"Program finished with exit code {code}")
